0e25839cb9da14d91cfa419713fd7bed83654acc
Gani Hari <panthaganihari1@gmail.com>
2026-03-28 13:44:39 +0530

    api

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:8080/api' });
+const API = axios.create({ baseURL: 'https://egf-library-backend.onrender.com/api' });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
